d7373f9f447c49aed1b0bf86742adb6ddbce5502
Frank Pinnola <frankie4pinnola@gmail.com>
2021-09-11 18:16:32 -0400

    Added styling/image/svg support

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import './index.css'
+import SQUID from './squid.png'
+import GEAR from './gear.svg'
+
 export const App = () => {
-    return <h1>React Typescript Application</h1>
+    return ( 
+    <>
+        <h1>React Typescript Application</h1>
+        <img src={SQUID} alt="SQUID" width="300" height="200" />
+        <img src={GEAR} alt="GEAR" width="300" />
+    </>
+    )
 }
