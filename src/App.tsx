dc692bc5e71363f8e8c8f10091a2518de5c8a008
Frank Pinnola <frankie4pinnola@gmail.com>
2021-09-12 17:00:58 -0400

    Set up prettier

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import GEAR from './gear.svg'
 import { ClickCounter } from './ClickCounter'
 
 export const App = () => {
-    return ( 
-    <>
-        <h1>React Typescript Application</h1>
-        <img src={SQUID} alt="SQUID" width="300" height="200" />
-        <img src={GEAR} alt="GEAR" width="300" />
-        <ClickCounter />
-    </>
-    )
+	return (
+		<>
+			<h1>React Typescript Application</h1>
+			<img src={SQUID} alt='SQUID' width='300' height='200' />
+			<img src={GEAR} alt='GEAR' width='300' />
+			<ClickCounter />
+		</>
+	)
 }
