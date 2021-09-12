1b718cf55b79113e371ad22c4c32c8e82ccc577f
Frank Pinnola <frankie4pinnola@gmail.com>
2021-09-11 21:30:08 -0400

    Added react-refresh-webpack-plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './index.css'
 import SQUID from './squid.png'
 import GEAR from './gear.svg'
+import { ClickCounter } from './ClickCounter'
 
 export const App = () => {
     return ( 
@@ -8,6 +9,7 @@ export const App = () => {
         <h1>React Typescript Application</h1>
         <img src={SQUID} alt="SQUID" width="300" height="200" />
         <img src={GEAR} alt="GEAR" width="300" />
+        <ClickCounter />
     </>
     )
 }
